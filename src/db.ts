99ae74afc4574f780dc76e60c99745ae9bb042a8
Hafidz <hafidzkarim18@gmail.com>
2024-12-04 12:46:26 +0700

    tugas tapi masih kosongan

---
+++ src/db.ts
@@ -0,0 +1,9 @@
+import { Pool } from "pg";
+
+const pool = new Pool ({
+    user: "postgres.xajgqatmlarqhwqairwn",
+    host: "aws-0-ap-southeast-1.pooler.supabase.com",
+    
+})
+
+user=postgres.xajgqatmlarqhwqairwn password=[YOUR-PASSWORD] host=aws-0-ap-southeast-1.pooler.supabase.com port=6543 dbname=postgres
